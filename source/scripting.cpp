30f677b8ed1120faf35e900dbb305cfbcc21419a
SpaceManiac <tad.hardesty@platymuus.com>
2011-10-19 03:42:36 -0500

    Make console show more backlog and allow scrolling

--- source/scripting.cpp
+++ source/scripting.cpp
@@ -7,12 +7,17 @@
 #include <vector>
 using namespace std;
 
+#define CONSOLE_SIZE (15)
+#define LINE_HEIGHT (14)
+
 struct console_t {
 	bool active, flash;
-	vector<string> backlog;
 	
 	string entry;
 	string::iterator entryIter;
+	
+	vector<string> backlog;
+	size_t backlogPos;
 	
 	vector<string> entryHistory;
 	size_t historyPos;
@@ -23,6 +28,8 @@ extern mfont_t *gameFont[2]; // the fonts, for FontWidth - see display.cpp
 
 void LogConsole(std::string s)
 {
+	bool move = console.backlogPos >= console.backlog.size() - CONSOLE_SIZE - 1;
+	
 	for (size_t i = 1; i < s.length(); ++i)
 	{
 		int len = FontStrLen(s.substr(0, i).c_str(), gameFont[1]);
@@ -34,7 +41,14 @@ void LogConsole(std::string s)
 			i = 1;
 		}
 	}
+	
 	console.backlog.push_back(s);
+	
+	while (console.backlog.size() > CONSOLE_SIZE && console.backlog[0] == "") {
+		console.backlog.erase(console.backlog.begin());
+	}
+		
+	if (move) console.backlogPos = console.backlog.size() - CONSOLE_SIZE;
 }
 
 void InitScripting()
@@ -54,9 +68,14 @@ void InitScripting()
 	void* data = LoadResource(NULL, resource);
 	interp->eval(string((char*)data, size));
 	
+	// configure console
 	console.active = false;
 	console.entryIter = console.entry.begin();
+	
+	for (int i = 0; i < CONSOLE_SIZE - 1; ++i) console.backlog.push_back("");
 	console.backlog.push_back("Tcl console initialized");
+	console.backlogPos = 0;
+	
 	console.entryHistory.push_back("# Top of history");
 	console.historyPos = 1;
 }
@@ -67,10 +86,10 @@ void ExitScripting()
 	interp = NULL;
 }
 
-byte ScriptKeyPressed(int key)
+byte ScriptKeyPressed(int keycode)
 {
-	char ch = key & 0xff;
-	int scan = key >> 8;
+	char ch = keycode & 0xff;
+	int scan = keycode >> 8;
 
 	if (scan == KEY_TILDE || scan == KEY_BACKQUOTE)
 	{
@@ -124,13 +143,24 @@ byte ScriptKeyPressed(int key)
 		}
 		else if (scan == KEY_UP)
 		{
-			if (console.historyPos > 0) console.historyPos--;
-			console.entry = console.entryHistory[console.historyPos];
-			console.entryIter = console.entry.end();
+			if (key[KEY_LCONTROL] || key[KEY_RCONTROL])
+			{
+				if (console.backlogPos > 0) console.backlogPos--;
+			}
+			else
+			{
+				if (console.historyPos > 0) console.historyPos--;
+				console.entry = console.entryHistory[console.historyPos];
+				console.entryIter = console.entry.end();
+			}
 		}
 		else if (scan == KEY_DOWN)
 		{
-			if (console.historyPos < console.entryHistory.size())
+			if (key[KEY_LCONTROL] || key[KEY_RCONTROL])
+			{
+				if (console.backlogPos < console.backlog.size() - CONSOLE_SIZE) console.backlogPos++;
+			}
+			else if (console.historyPos < console.entryHistory.size())
 			{
 				if (++console.historyPos == console.entryHistory.size())
 				{
@@ -158,26 +188,28 @@ void RenderConsole(MGLDraw* mgl)
 	if (!console.active) return;
 	console.flash = !console.flash;
 
-	mgl->FillBox(322, 2, 638, 162, 10);
-	mgl->FillBox(324, 4, 636, 144, 20);
-	mgl->FillBox(324, 146, 636, 160, 20);
-	Print(326, 147, console.entry.c_str(), 0, 1);
+	mgl->FillBox(322, 2, 638, 8 + (CONSOLE_SIZE+1)*LINE_HEIGHT, 10);
+	mgl->FillBox(324, 4, 636, 4 + CONSOLE_SIZE*LINE_HEIGHT, 20);
+	mgl->FillBox(324, 6 + CONSOLE_SIZE*LINE_HEIGHT, 636, 6 + (CONSOLE_SIZE+1)*LINE_HEIGHT, 20);
+	Print(326, 7 + CONSOLE_SIZE*LINE_HEIGHT, console.entry.c_str(), 0, 1);
 	
 	if (console.flash)
 	{
 		// draw the caret
 		int x = 325 + FontStrLen(console.entry.substr(0, console.entryIter - console.entry.begin()).c_str(), gameFont[1]);
-		mgl->Box(x, 147, x, 147+12, 31);
+		mgl->Box(x, 7 + CONSOLE_SIZE*LINE_HEIGHT, x, 5 + (CONSOLE_SIZE+1)*LINE_HEIGHT, 31);
 	}
+	
+	// draw up/down "arrows" if log is scrollable
+	if (console.backlogPos > 0)
+		mgl->FillBox(627, 7 + CONSOLE_SIZE*LINE_HEIGHT, 635, 10+CONSOLE_SIZE*LINE_HEIGHT, 10);
+	if (console.backlogPos < console.backlog.size() - CONSOLE_SIZE)
+		mgl->FillBox(627, 16 + CONSOLE_SIZE*LINE_HEIGHT, 635, 19+CONSOLE_SIZE*LINE_HEIGHT, 10);
 
-	int start = console.backlog.size() - 10;
-	if (start < 0) start = 0;
-	int y = 5 + 14 * (10 - console.backlog.size());
-	if (y < 5) y = 5;
-
-	for (size_t i = start; i < console.backlog.size(); ++i)
+	int y = 5;
+	for (size_t i = console.backlogPos; i < console.backlogPos + CONSOLE_SIZE; ++i)
 	{
 		Print(326, y, console.backlog[i].c_str(), 0, 1);
-		y += 14;
+		y += LINE_HEIGHT;
 	}
 }
