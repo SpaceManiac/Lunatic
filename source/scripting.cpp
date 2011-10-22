17b4ae36e1cc5a30fc2e6a9b155a54268ccc79df
SpaceManiac <tad.hardesty@platymuus.com>
2011-10-21 20:57:23 -0500

    Move AI specifications to monster types instead of giant case statement; remove special case for not compiling monster2 and monster3.cpp; general cleanup

--- source/scripting.cpp
+++ source/scripting.cpp
@@ -10,15 +10,16 @@ using namespace std;
 #define CONSOLE_SIZE (15)
 #define LINE_HEIGHT (14)
 
-struct console_t {
+struct console_t
+{
 	bool active, flash;
-	
+
 	string entry;
 	string::iterator entryIter;
-	
+
 	vector<string> backlog;
 	size_t backlogPos;
-	
+
 	vector<string> entryHistory;
 	size_t historyPos;
 } console;
@@ -29,7 +30,7 @@ extern mfont_t *gameFont[2]; // the fonts, for FontWidth - see display.cpp
 void LogConsole(std::string s)
 {
 	bool move = console.backlogPos >= console.backlog.size() - CONSOLE_SIZE - 1;
-	
+
 	for (size_t i = 1; i < s.length(); ++i)
 	{
 		int len = FontStrLen(s.substr(0, i).c_str(), gameFont[1]);
@@ -41,13 +42,14 @@ void LogConsole(std::string s)
 			i = 1;
 		}
 	}
-	
+
 	console.backlog.push_back(s);
-	
-	while (console.backlog.size() > CONSOLE_SIZE && console.backlog[0] == "") {
+
+	while (console.backlog.size() > CONSOLE_SIZE && console.backlog[0] == "")
+	{
 		console.backlog.erase(console.backlog.begin());
 	}
-		
+
 	if (move) console.backlogPos = console.backlog.size() - CONSOLE_SIZE;
 }
 
@@ -55,27 +57,27 @@ void InitScripting()
 {
 	interp = new tcl::interpreter();
 	// safeify
-	interp->def<void, std::string>("puts", LogConsole);
+	interp->def<void, std::string > ("puts", LogConsole);
 	interp->eval("rename exit {}");
-	
+
 	// define lm_ functions
 	interp->eval("namespace eval lm_ {}");
 	interp->def<byte, byte, int, int>("lm_::PlayerGetItem", PlayerGetItem);
-	
+
 	// execute embedded init.tcl
 	HRSRC resource = FindResource(NULL, "init_tcl", "SCRIPT");
 	dword size = SizeofResource(NULL, resource);
 	void* data = LoadResource(NULL, resource);
-	interp->eval(string((char*)data, size));
-	
+	interp->eval(string((char*) data, size));
+
 	// configure console
 	console.active = false;
 	console.entryIter = console.entry.begin();
-	
+
 	for (int i = 0; i < CONSOLE_SIZE - 1; ++i) console.backlog.push_back("");
 	console.backlog.push_back("Tcl console initialized");
 	console.backlogPos = 0;
-	
+
 	console.entryHistory.push_back("# Top of history");
 	console.historyPos = 1;
 }
@@ -114,10 +116,10 @@ byte ScriptKeyPressed(int keycode)
 				s = e.what();
 			}
 			if (s != "") LogConsole(s);
-			
+
 			console.entryHistory.push_back(console.entry);
 			console.historyPos = console.entryHistory.size();
-			
+
 			console.entry.clear();
 			console.entryIter = console.entry.begin();
 		}
@@ -188,23 +190,23 @@ void RenderConsole(MGLDraw* mgl)
 	if (!console.active) return;
 	console.flash = !console.flash;
 
-	mgl->FillBox(322, 2, 638, 8 + (CONSOLE_SIZE+1)*LINE_HEIGHT, 10);
+	mgl->FillBox(322, 2, 638, 8 + (CONSOLE_SIZE + 1) * LINE_HEIGHT, 10);
 	mgl->FillBox(324, 4, 636, 4 + CONSOLE_SIZE*LINE_HEIGHT, 20);
-	mgl->FillBox(324, 6 + CONSOLE_SIZE*LINE_HEIGHT, 636, 6 + (CONSOLE_SIZE+1)*LINE_HEIGHT, 20);
+	mgl->FillBox(324, 6 + CONSOLE_SIZE*LINE_HEIGHT, 636, 6 + (CONSOLE_SIZE + 1) * LINE_HEIGHT, 20);
 	Print(326, 7 + CONSOLE_SIZE*LINE_HEIGHT, console.entry.c_str(), 0, 1);
-	
+
 	if (console.flash)
 	{
 		// draw the caret
 		int x = 325 + FontStrLen(console.entry.substr(0, console.entryIter - console.entry.begin()).c_str(), gameFont[1]);
-		mgl->Box(x, 7 + CONSOLE_SIZE*LINE_HEIGHT, x, 5 + (CONSOLE_SIZE+1)*LINE_HEIGHT, 31);
+		mgl->Box(x, 7 + CONSOLE_SIZE*LINE_HEIGHT, x, 5 + (CONSOLE_SIZE + 1) * LINE_HEIGHT, 31);
 	}
-	
+
 	// draw up/down "arrows" if log is scrollable
 	if (console.backlogPos > 0)
-		mgl->FillBox(627, 7 + CONSOLE_SIZE*LINE_HEIGHT, 635, 10+CONSOLE_SIZE*LINE_HEIGHT, 10);
+		mgl->FillBox(627, 7 + CONSOLE_SIZE * LINE_HEIGHT, 635, 10 + CONSOLE_SIZE * LINE_HEIGHT, 10);
 	if (console.backlogPos < console.backlog.size() - CONSOLE_SIZE)
-		mgl->FillBox(627, 16 + CONSOLE_SIZE*LINE_HEIGHT, 635, 19+CONSOLE_SIZE*LINE_HEIGHT, 10);
+		mgl->FillBox(627, 16 + CONSOLE_SIZE * LINE_HEIGHT, 635, 19 + CONSOLE_SIZE * LINE_HEIGHT, 10);
 
 	int y = 5;
 	for (size_t i = console.backlogPos; i < console.backlogPos + CONSOLE_SIZE; ++i)
